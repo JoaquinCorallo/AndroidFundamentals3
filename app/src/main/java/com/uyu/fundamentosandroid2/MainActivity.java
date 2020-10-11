d0f677eebb73b0ce8ad7a16e0782dbfb3030528f
Joaquin <joaquincorallofabiano@gmail.com>
2020-10-11 18:00:04 -0300

    Initial commit

---
+++ app/src/main/java/com/uyu/fundamentosandroid2/MainActivity.java
@@ -0,0 +1,89 @@
+package com.uyu.fundamentosandroid2;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.DialogFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+    public RecyclerView listaMascotas;
+    ArrayList<Mascota> mascotas;
+
+    public Button btnMove;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        androidx.appcompat.widget.Toolbar toolbar = (Toolbar) findViewById(R.id.miActionBar);
+        setSupportActionBar(toolbar);
+
+        listaMascotas = (RecyclerView) findViewById(R.id.rvmascotas);
+
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+
+        listaMascotas.setLayoutManager(llm);
+        inicializarListaContactos();
+        inicializarAdaptador();
+
+        btnMove = findViewById(R.id.BtnActivityOne);
+
+        btnMove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MoveToActivityTwo();
+            }
+        });
+    }
+
+    private void MoveToActivityTwo(){
+        Intent intent = new Intent(MainActivity.this, confirmacion.class);
+        startActivity(intent);
+    }
+
+    public void inicializarAdaptador(){
+        mascotasadaptador adaptador = new mascotasadaptador(mascotas);
+        adaptador = new mascotasadaptador(mascotas);
+        listaMascotas.setAdapter(adaptador);
+    }
+
+    public void inicializarListaContactos(){
+        mascotas = new ArrayList<Mascota>();
+        mascotas.add(new Mascota(R.drawable.mono, "Mono"));
+        mascotas.add(new Mascota(R.drawable.pez, "pez"));
+        mascotas.add(new Mascota(R.drawable.gato, "gato"));
+        mascotas.add(new Mascota(R.drawable.vibora, "vibora"));
+        mascotas.add(new Mascota(R.drawable.perro, "perro"));
+        mascotas.add(new Mascota(R.drawable.huron, "huron"));
+        //mascotas.add(new Mascota(R.drawable.conejo, "conejo"));
+        //mascotas.add(new Mascota(R.drawable.hamster, "hamster"));
+        mascotas.add(new Mascota(R.drawable.camaleon, "camaleon"));
+    }
+
+
+
+
+}
+
+
